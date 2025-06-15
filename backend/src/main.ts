bcd2b342929ca08c13bfcfd4b0cafd8a221f2664
JesusMiir <jesusmercadalmir@gmail.com>
2025-06-15 15:18:04 +0200

    comment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://steam-clone-jesus.s3-website.eu-central-1.amazonaws.com',
+    origin: 'https://www.steam-project.es',
     credentials: true, // solo si usas cookies o headers con auth
   });
 
