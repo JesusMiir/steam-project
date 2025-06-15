39d60c9a1f08c33e565fe5a87d9d7e0d7c71f816
Carlos Andonaegui <charlires@gmail.com>
2025-06-15 08:06:05 -0600

    update to support docker compose for local dev env (#1)
    * update to support docker compose for local dev env
    
    * add comment for new env variables

--- frontend/src/services/cart.jsx
+++ frontend/src/services/cart.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'https://api.steam-project.es';
+// const API_URL = import.meta.env.BACKEND_HOST;
+const API_URL = 'http://localhost:3000';
 
 export const getCart = (userId) => axios.get(`${API_URL}/cart/${userId}`);
 export const checkoutCart = (userId) =>
--- frontend/src/services/games.jsx
+++ frontend/src/services/games.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'https://api.steam-project.es';
+// const API_URL = import.meta.env.BACKEND_HOST;
+const API_URL = 'http://localhost:3000';
 
 export const getGames = () => axios.get(`${API_URL}/games`);
 export const addToCart = (userId, gameId) =>
