d123b6d85f2465a3d5b8f02588c654cfe73fc56b
Carlos Andonaegui <charlires@gmail.com>
2025-06-15 08:05:21 -0600

    add comment for new env variables

--- frontend/src/services/games.jsx
+++ frontend/src/services/games.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+// const API_URL = import.meta.env.BACKEND_HOST;
 const API_URL = 'http://localhost:3000';
 
 export const getGames = () => axios.get(`${API_URL}/games`);
--- frontend/src/services/library.jsx
+++ frontend/src/services/library.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+
+// const API_URL = import.meta.env.BACKEND_HOST;
 const API_URL = 'http://localhost:3000';
 
 export const getLibrary = (userId) => axios.get(`${API_URL}/library/${userId}`);
