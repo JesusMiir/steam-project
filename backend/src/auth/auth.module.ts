005d85b022858dd4ec20cc0e3a0641ca84d0f100
Jesús Mercadal Mir <42605115+JesusMiir@users.noreply.github.com>
2025-08-17 16:30:56 +0200

    creat testing files (#15)

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,21 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
-import { GoogleStrategy } from './google.strategy';
-import { PassportModule } from '@nestjs/passport';
-import { PrismaModule } from '../prisma/prisma.module';
-import { JwtModule } from '@nestjs/jwt';
+import { Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { GoogleStrategy } from "./google.strategy";
+import { PassportModule } from "@nestjs/passport";
+import { PrismaModule } from "../prisma/prisma.module";
+import { JwtModule } from "@nestjs/jwt";
+import { JwtStrategy } from "./jwt.strategy";
 
 @Module({
   imports: [
     PassportModule,
     PrismaModule,
     JwtModule.register({
-      secret: process.env.JWT_SECRET || 'default-secret',
-      signOptions: { expiresIn: '1d' },
+      secret: process.env.JWT_SECRET || "default-secret",
+      signOptions: { expiresIn: "1d" },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, GoogleStrategy],
+  providers: [AuthService, GoogleStrategy, JwtStrategy],
 })
 export class AuthModule {}
